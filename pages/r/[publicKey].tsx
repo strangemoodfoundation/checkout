587c9b02ca0a15ddc7acb16418bd29bd8a8cb75e
Evan Conrad <evan@roomservice.dev>
2022-03-01 13:58:06 -0800

    fix checkout

--- pages/r/[publicKey].tsx
+++ pages/r/[publicKey].tsx
@@ -44,38 +44,38 @@ export default function Checkout(props: {
         <div className="">share</div>
       </div>
       <div className="flex flex-row w-full pb-4 justify-start start">
-        <div className="flex flex-col  bg-gray-800 flex-1 w-full">
+        <div className="flex flex-col dark:bg-gray-800 bg-gray-100 flex-1 w-full">
           <img
             src={props.listing.metadata.primaryImage}
             className=" w-full object-contain"
           />
-          <p className="text-sm px-4 py-2 text-gray-200">
+          <p className="text-sm px-4 py-2 opacity-80">
             {props.listing.metadata.description}
           </p>
         </div>
       </div>
 
-      <div className="relative bg-gray-800 flex flex-row justify-between items-center p-4 w-full rounded-sm">
+      <div className="relative dark:bg-gray-800 bg-gray-100 flex flex-row justify-between items-center p-4 w-full rounded-sm">
         <div className="mr-4 flex flex-col">
           <div className="font-bold text-xl">
             Buy {props.listing.metadata.name}
           </div>
           <a
-            className="opacity-50 underline"
+            className="opacity-50 underline text-xs"
             href={`https://explorer.strangemood.com/address/somelistingid`}
           >
-            {`sol://somelistingid`}
+            {`sol://${"foo"}`}
           </a>
         </div>
         <div>
           <div className="flex justify-end pr-4">
             <AppleSVG className="h-5 opacity-80 top-2 relative" />
           </div>
-          <div className="relative flex rounded justify-center flex-row items-center top-10 bg-black pl-4 pr-1 py-1">
+          <div className="relative flex rounded justify-center flex-row items-center top-10 dark:bg-black  bg-white pl-4 dark:pr-1 dark:py-1 border dark:border-0 border-black">
             <div className="mr-4 text-sm font-mono opacity-80">
               <div>${price}</div>
             </div>
-            <button className="flex px-4 py-2 transition-all bg-green-700 hover:bg-green-500 rounded-sm font-medium">
+            <button className="flex  px-4 py-2 transition-all  text-green-800 dark:text-gray-900 bg-green-400 dark:bg-green-500 dark:hover:bg-green-400 dark:rounded-sm rounded-r-sm font-medium">
               Checkout
             </button>
           </div>
