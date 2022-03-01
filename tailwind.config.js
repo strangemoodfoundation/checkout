ae8f946c1bff402e3c815fe9c3094e987f4170b7
Evan Conrad <evan@roomservice.dev>
2022-02-28 17:20:59 -0800

    add start of checkout

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "media",
+  theme: {
+    colors: {
+      ...colors,
+      gray: colors.zinc,
+      blue: colors.sky,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
