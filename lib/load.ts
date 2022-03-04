c27f2268f5178a37e7b5567f48e27efd27d620ab
Evan Conrad <evan@roomservice.dev>
2022-03-04 13:23:22 -0800

    fix prerendering

--- lib/load.ts
+++ lib/load.ts
@@ -24,7 +24,7 @@ export interface Listing {
   }; // The Solana account data
   cid: string; // The CID of the IPFS content
   currency: string; // The string of the mint
-  metadata: ListingMetadata; // The IPFS metadata
+  metadata?: ListingMetadata; // The IPFS metadata
 }
 
 export async function solanaPrice() {
@@ -111,13 +111,19 @@ export async function loadListing(
     }
   `;
 
-  const data = await request(
-    `https://www.openmetagraph.com/api/graphql?schema=QmUmLdYHHAqDYNnRGeKbHg4pxocFV1VAZuuHuRvdNiY1Bb&schema=QmRnQhScsXQvMtWNLNQJ3Hbxn2NCVBvaU4PPp37CYX54pr&schema=Qmd3ChXtpCAQqRkAySdJz1HerUoCnVJQT1aBTgizYLximQ`,
-    query,
-    {
-      key: uri,
-    }
-  );
+  let data;
+
+  try {
+    data = await request(
+      `https://www.openmetagraph.com/api/graphql?schema=QmUmLdYHHAqDYNnRGeKbHg4pxocFV1VAZuuHuRvdNiY1Bb&schema=QmRnQhScsXQvMtWNLNQJ3Hbxn2NCVBvaU4PPp37CYX54pr&schema=Qmd3ChXtpCAQqRkAySdJz1HerUoCnVJQT1aBTgizYLximQ`,
+      query,
+      {
+        key: uri,
+      }
+    );
+  } catch (err) {
+    console.error(err);
+  }
 
   const listing: Listing = {
     publicKey: publicKey,
@@ -137,13 +143,15 @@ export async function loadListing(
       mint: account.mint.toString(),
     },
     cid: uri,
-    metadata: {
-      name: data.get.name,
-      description: data.get.description,
-      tagline: "todo",
-      images: data.get.images.map((image: any) => image.uri),
-      primaryImage: data.get.primaryImage.uri,
-    },
+    metadata: data
+      ? {
+          name: data.get.name,
+          description: data.get.description,
+          tagline: "todo",
+          images: data.get.images.map((image: any) => image.uri),
+          primaryImage: data.get.primaryImage.uri,
+        }
+      : undefined,
   };
 
   return listing;
